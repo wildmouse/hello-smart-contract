751cc4eaee9f96d5bb10a2b87f1c5477ae3f3458
Tsukasa Noguchi <wildmouse0813@gmail.com>
2020-07-19 17:00:08 +0900

    Add migrating to goerli

--- truffle-config.js
+++ truffle-config.js
@@ -1,5 +1,7 @@
 const path = require("path");
 
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
@@ -14,6 +16,16 @@ module.exports = {
       host: "127.0.0.1",
       port: 8545,
       network_id: "*",
+    },
+    goerli: {
+      provider: () => {
+        const mnemonic = process.env["MNEMONIC"]
+        return new HDWalletProvider(
+          mnemonic,
+          "https://goerli.infura.io/v3/41a7edba555441859a807aeb290d158b"
+        )
+      },
+      network_id: "5"
     }
   }
 };
