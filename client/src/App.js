9b18f4e87ec968323033ff43efe68e93615d9f27
Tsukasa Noguchi <wildmouse0813@gmail.com>
2020-07-19 18:25:15 +0900

    Add updating greeting

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,11 @@
-import React, { Component } from "react";
+import React, {Component} from "react";
 import GreeterContract from "./contracts/Greeter.json";
 import getWeb3 from "./getWeb3";
 
 import "./App.css";
 
 class App extends Component {
-  state = { greeting: "", web3: null, accounts: null, contract: null };
+  state = {greeting: "", newGreeting: "", web3: null, accounts: null, contract: null};
 
   componentDidMount = async () => {
     try {
@@ -25,7 +25,7 @@ class App extends Component {
 
       // Set web3, accounts, and contract to the state, and then proceed with an
       // example of interacting with the contract's methods.
-      this.setState({ web3, accounts, contract: instance }, this.runExample);
+      this.setState({web3, accounts, contract: instance}, this.getGreeting);
     } catch (error) {
       // Catch any errors for any of the above operations.
       alert(
@@ -35,13 +35,22 @@ class App extends Component {
     }
   };
 
-  runExample = async () => {
-    const { accounts, contract } = this.state;
+  getGreeting = async () => {
+    const {contract} = this.state;
 
     const response = await contract.methods.greeting().call();
 
-    this.setState({greeting: response });
+    this.setState({greeting: response});
   };
+
+  updateGreeting = async () => {
+    const {accounts, contract, newGreeting} = this.state;
+
+    contract.methods.updateGreeting(newGreeting).send({ from: accounts[0]})
+      .catch(err => {
+        console.error(err)
+      })
+  }
 
   render() {
     if (!this.state.web3) {
@@ -51,6 +60,11 @@ class App extends Component {
       <div className="App">
         <h1>Greeter</h1>
         <div>{this.state.greeting}</div>
+        <input
+          value={this.state.newGreeting}
+          onChange={e => this.setState({newGreeting: e.target.value})}
+        />
+        <button onClick={this.updateGreeting}>Update Greeting</button>
       </div>
     );
   }
