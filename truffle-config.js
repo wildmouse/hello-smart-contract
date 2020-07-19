803183822e8196f0e62651a2af25c2ae75516053
Tsukasa Noguchi <wildmouse0813@gmail.com>
2020-07-19 09:43:00 +0900

    Add ganache network as local

--- truffle-config.js
+++ truffle-config.js
@@ -9,6 +9,11 @@ module.exports = {
       host: "127.0.0.1",
       port: 9545,
       network_id: "*"
+    },
+    local: {
+      host: "127.0.0.1",
+      port: 8545,
+      network_id: "*",
     }
   }
 };
